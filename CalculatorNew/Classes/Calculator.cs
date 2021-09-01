19f3629dba63e8392afa91344c5184e8615002d1
Holiks-Serbuchev <dr.serbuchev@mail.ru>
2021-09-01 20:54:03 +0300

    Default

---
+++ CalculatorNew/Classes/Calculator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorNew.Classes
+{
+    public class Calculator
+    {
+        List<string> commands = new List<string>() { "Select option:", "1. Addition", "2. Subtraction", "3. Multiplication", "4. Division" };
+        double firstNum, secondNum = 0;
+        public Calculator(double firstValue, double secondValue)
+        {
+            firstNum = firstValue;
+            secondNum = secondValue;
+        }
+        public double Operation(double operation)
+        {
+            double result = 0;
+            if (operation == 1)
+                result = firstNum + secondNum;
+            else if (operation == 2)
+                result = firstNum - secondNum;
+            else if (operation == 3)
+                result = firstNum * secondNum;
+            else if (operation == 4)
+                result = firstNum / secondNum;
+            else
+                Console.WriteLine("Incorrect value!");
+            return result;
+        }
+        public void PrintCommands()
+        {
+            foreach (var item in commands)
+                Console.WriteLine(item);
+        }
+        public void EndPrintCommands(out bool saveNumber)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            bool check = false;
+            Console.WriteLine("1. Continue the program and clear the values");
+            Console.WriteLine("2. Continue working with number");
+            Console.WriteLine("3. Exit");
+            string value = Console.ReadLine();
+            if (value == "3")
+                Environment.Exit(1);
+            else if (value == "2")
+                check = true;
+            saveNumber = check;
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
---
+++ CalculatorNew/Program.cs
@@ -0,0 +1,46 @@
+﻿using CalculatorNew.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorNew
+{
+    class Program
+    {
+        static bool saveNumber = false;
+        static double answer = 0;
+        static void Main(string[] args)
+        {
+            double firstNum, secondNum;
+            while (true)
+            {
+                if (saveNumber == false)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Enter the first number:");
+                    firstNum = ConvertNumber(Console.ReadLine());
+                }
+                else
+                {
+                    Console.WriteLine("First number:" + answer);
+                    firstNum = answer;
+                }
+                Console.WriteLine("Enter the second number:");
+                secondNum = ConvertNumber(Console.ReadLine());
+                Calculator calculator = new Calculator(firstNum, secondNum);
+                calculator.PrintCommands();
+                answer = calculator.Operation(ConvertNumber(Console.ReadLine()));
+                Console.WriteLine("Answer: " + answer + "\n------------------------");
+                calculator.EndPrintCommands(out saveNumber);
+            }
+        }
+        private static double ConvertNumber(string number)
+        {
+            if (double.TryParse(number.Replace(".",","), out double newNum))
+                return newNum;
+            return 0;
+        }
+    }
+}
